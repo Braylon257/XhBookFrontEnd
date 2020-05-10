3cb10b4b86e630ae1f43a5a6b2b915895b2083d9
王日红 <3291765592@qq.com>
2020-05-10 16:04:41 +0800

    王日红

---
+++ XhBookIndex/src/main/resources/register/register.js
@@ -0,0 +1,8 @@
+let dv = my$("login-form");
+let input1 = dv.document.getElementsByTagName("input");
+let spa = dv.document.getElementsByTagName("span");
+function f() {
+    if(input1.value===""){
+        spa ==="";
+    }
+}
